54c8d8d2e2200923610b8416744a14ed9405ffd6
Etienne Lemonnier <56196021+elemonnier@users.noreply.github.com>
2021-11-11 11:24:51 +0100

    Create Exercice1.c

---
+++ TP3/Exercice1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int punition1 (int a)
+{
+        for (a=0; a<10; a++)
+        {
+                printf("Je dois apprendre mes cours\n");
+        }
+}
+
+int main ()
+{
+        int a;
+
+        a = punition1 (a);
+
+        return EXIT_SUCCESS;
+}
