3419b376c04b3b36b4054dcb600fdd13a3fbdece
Etienne Lemonnier <56196021+elemonnier@users.noreply.github.com>
2021-11-11 11:10:21 +0100

    Create Exercice9.c

---
+++ TP1/Exercice9.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int a, b, c;
+        printf("Entrez la somme > ");
+        scanf("%d\n", &a);
+        printf("%d", &a);
+        printf(" = ");
+        if (a >= 100)
+        {
+                (b=a-100);
+        }
+        else
+        {
+                 (b=a);
+        }
+        if (b >= 50)
+        {
+                (c=b-50);
+        }
+        else
+        {
+                (c=b);
+        }
+        if (c >= 20)
+        {
+                (d=c-20);
+        }
+        else
+        {
+                (d=c);
+        }
+        if (d >= 10)
+        {
+                (e=d-10);
+        }
+        else
+        {
+                (e=d);
+        }
+        if (e >= 5)
+        {
+                (f=e-5)
+        }
+        else
+        {
+                (f=e)
+        }
+}
