771ee4e783e52f7d0d9ae9ec4f6f75e91789ad4c
Etienne Lemonnier <56196021+elemonnier@users.noreply.github.com>
2021-11-11 11:30:22 +0100

    Create Exercice9bis.c

---
+++ TP3/Exercice9bis.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int CoefficientBinomeRecursive (int n, int p)
+{
+        if (p==0)
+        {
+                return 1;
+        }
+        if (p==n)
+        {
+                return 1;
+        }
+
+}
+
+int main ()
+{
+        int a, p;
+}
