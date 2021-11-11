b3798b757bb1639f0321b9b4fcef11d7fe426abf
Etienne Lemonnier <56196021+elemonnier@users.noreply.github.com>
2021-11-11 11:51:05 +0100

    Create Exercice4.c

---
+++ TP7/Exercice4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void Palindrome ()
+{
+
+}
+
+int main (char argc, char** argv)
+{
+        if (argc == 0)
+        {
+                printf("Entrez une chaîne : ");
+                fgets(chaine, 32, stdin);
+                Palindrome(chaine);
+
+                return 0;
+        }
+        if (argc == 1)
+        {
+
+        }
+}
